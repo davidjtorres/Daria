8b43911d00380b9ddeb54b311fbace066b7a8037
David Torres <davidj_torres@hotmail.com>
2025-08-09 15:12:58 -0500

    adding TransactionCategory enum

---
+++ constants.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class TransactionCategory(Enum):
+    TECHNOLOGY = "technology"
+    SUBSCRIPTIONS = "subscriptions"
+    FOOD = "food"
+    SHOPPING = "shopping"
+    GROCERIES = "groceries"
+    TRANSPORTATION = "transportation"
+    ENTERTAINMENT = "entertainment"
+    HEALTH = "health"
+    UTILITIES = "utilities"
